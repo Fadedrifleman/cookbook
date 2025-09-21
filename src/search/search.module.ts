4c9dde7a2413d9fbb6bb11cd6781a9474bdab153
shivam uttam <fadedrifleman@gmail.com>
2025-09-21 10:37:13 +0530

    feat: configure services to use environment variables for production

--- src/search/search.module.ts
+++ src/search/search.module.ts
@@ -8,11 +8,19 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     ElasticsearchModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: () => ({
-        node: 'http://localhost:9200',
-        maxRetries: 10,
-        requestTimeout: 60000,
-      }),
+      // The factory will now inject the ConfigService to read environment variables
+      useFactory: (configService: ConfigService) => {
+        // Read the production ELASTICSEARCH_URL from the environment
+        const esUrl = configService.get<string>('ELASTICSEARCH_URL');
+
+        return {
+          // If the esUrl exists (in production), use it.
+          // Otherwise (in local dev), fall back to the Docker container URL.
+          node: esUrl || 'http://localhost:9200',
+          maxRetries: 10,
+          requestTimeout: 60000,
+        };
+      },
     }),
   ],
   providers: [SearchService],
